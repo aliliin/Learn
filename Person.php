37fc75eac2991eb9b918e286ee09b906595df4ba
aliliin <fonliny@gmail.com>
2017-04-30 11:42:46 +0800

    PHP get set 学习

---
+++ Person.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ali
+ * Date: 2017/4/30
+ * Time: 上午11:30
+ */
+class Person
+{
+    public $name;
+    public $age;
+    public function __construct($name)
+    {
+        $this->name = $name;
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function setAge($age)
+    {
+        if ($age < 18){
+            throw new ErrorException('not old enough');
+        }
+         $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+$gao = new Person( 'gaoyongli');
+$gao->setAge(30);
+$gao->age = 17;
+var_dump($gao->getAge());

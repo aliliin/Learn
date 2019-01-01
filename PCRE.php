e19d8e5de2c6e973ae5e009e19a8fc393eea484e
aliliin <phperali@gmail.com>
2019-01-01 16:33:33 +0800

    简单PHP练习题-正则匹配

--- PCRE.php
+++ PCRE.php
@@ -17,6 +17,12 @@ class Test
         return strlen(preg_replace('/[^aeiou]/i', '', $str));
         return preg_match_all('/[^aeiou]/i', $str);
     }
+    // 从字符串中删除特殊字符
+    // 创建一个带字符串的函数，删除所有“特殊”字符（例如！@＃$％^＆\ *）并返回新字符串。允许的唯一非字母数字字符是破折号 - ，下划线_和空格。
+    public static function removeSpecialCharacters($str)
+    {
+        return preg_replace('/[^A-Za-z0-9-\/ _]/', '', $str);
+    }
 }
 // 创建一个函数，该函数接受一个字符串并返回一个新字符串，删除所有元音。
 $str = "I have never seen a thin person drinking Diet Coke";
@@ -26,3 +32,6 @@ echo '<br/>';
 //  创建一个接受字符串并返回其中包含的元音数量（计数）的函数。
 $str = "Celebration";
 echo Test::countAeiouNumber($str);
+// 创建一个带字符串的函数，删除所有“特殊”字符（例如！@＃$％^＆\ *）并返回新字符串。允许的唯一非字母数字字符是破折号 - ，下划线_和空格。
+$str = "%fd76$fd(-)6GvKlO. ni kl lkk ";
+echo Test::removeSpecialCharacters($str);

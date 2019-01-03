4b5102f2441b6357f24b855435e3cad5589ffe53
aliliin <phperali@gmail.com>
2019-01-03 19:38:18 +0800

    简单PHP练习题-正则匹配纯数字

--- PCRE.php
+++ PCRE.php
@@ -23,6 +23,12 @@ class Test
     {
         return preg_replace('/[^A-Za-z0-9-\/ _]/', '', $str);
     }
+
+    // 验证字符串必须是4位数和6位的数字字符
+    public static function validationNumber($str)
+    {
+        return preg_match('/^(\d{6}|\d{4})$/', $str);
+    }
 }
 // 创建一个函数，该函数接受一个字符串并返回一个新字符串，删除所有元音。
 $str = "I have never seen a thin person drinking Diet Coke";
@@ -35,3 +41,7 @@ echo Test::countAeiouNumber($str);
 // 创建一个带字符串的函数，删除所有“特殊”字符（例如！@＃$％^＆\ *）并返回新字符串。允许的唯一非字母数字字符是破折号 - ，下划线_和空格。
 $str = "%fd76$fd(-)6GvKlO. ni kl lkk ";
 echo Test::removeSpecialCharacters($str);
+// 验证字符串必须是4位数和6位的纯数字字符
+// $str = '+234'; false    $str = '-23455', false $str ='123456'; true
+$str = '1235';
+echo Test::validationNumber($str);

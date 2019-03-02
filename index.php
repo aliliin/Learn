083a758403bcf827b699e1a596a62ba88e583ce2
aliliin <phperali@gmail.com>
2019-03-02 23:42:55 +0800

    练习

--- index.php
+++ index.php
@@ -8,7 +8,84 @@ use \Learn\Observer\UserObserver;
 use \Learn\TraitLearn\MiPhone;
 use \Learn\TraitLearn\SamsangPhone;
 use \Learn\TwoSum;
+
 echo '<pre/>';
+abstract class Shape
+{
+    protected $width  = 0;
+    protected $height = 0;
+    abstract public function getArea(): int;
+    public function render(int $area): void
+    {
+        //...
+    }
+}
+class Rectangle extends Shape
+{
+    public function setWidth(int $width): void
+    {
+        $this->width = $width;
+    }
+
+    public function setHeight(int $height): void
+    {
+        $this->height = $height;
+    }
+
+    public function getArea(): int
+    {
+        return $this->width * $this->height;
+    }
+}
+class Square extends Shape
+{
+    private $length = 0;
+
+    public function setLength(int $length): void
+    {
+        $this->length = $length;
+    }
+
+    public function getArea(): int
+    {
+        return pow($this->length, 2);
+    }
+}
+
+function renderLargeRectangles(array $rectangles): void
+{
+    foreach ($rectangles as $rectangle) {
+        if ($rectangle instanceof Square) {
+            $rectangle->setLength(5);
+        } elseif ($rectangle instanceof Rectangle) {
+            $rectangle->setWidth(4);
+            $rectangle->setHeight(5);
+        }
+
+        $area = $rectangle->getArea();
+        $rectangle->render($area);
+    }
+}
+
+$shapes = [new Rectangle(), new Rectangle(), new Square()];
+renderLargeRectangles($shapes);
+$nums   = [4, 2, 1];
+$number = count($nums);
+$i      = 1;
+$true   = 'true';
+foreach ($nums as $num) {
+    if ($i < $number) {
+        if ($num > $nums[$i]) {
+            $true = 'false';
+        } else {
+            $true = 'true';
+        }
+    }
+    $i++;
+}
+
+echo $true;
+die;
 $mi  = new MiPhone('miui');
 $san = new SamsangPhone();
 print_r($san);
